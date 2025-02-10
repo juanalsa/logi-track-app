b003ba52eece6d736db1cf442feaa90fc77b6129
Julian Alvarado Sanchez <juanalsa@gmail.com>
2025-02-10 03:31:12 -0500

    Add initial server setup with environment configuration and authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
+import { envs } from './config';
+import { AppRoutes } from './presentation/routes';
+import { Server } from './server';
 
-console.log("Test!!!");
+(() => {
+  main();
+})();
+
+async function main() {
+  new Server({ port: envs.PORT, routes: AppRoutes.routes }).start();
+}
